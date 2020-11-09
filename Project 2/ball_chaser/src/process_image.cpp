4de5aafeb44a044d9448bdaee6a99ca602eb30ba
Yi-Yuan Chiang <yychiang.vnu@gmail.com>
2020-11-09 22:17:06 +0800

    Update process_image.cpp

--- Project 2/ball_chaser/src/process_image.cpp
+++ Project 2/ball_chaser/src/process_image.cpp
@@ -1,11 +1,10 @@
+#include "ros/ros.h"
+#include "ball_chaser/DriveToTarget.h"
+#include <sensor_msgs/Image.h>
 #include <iostream>
 #include <algorithm>
 #include <vector>
 using namespace std;
-
-#include "ros/ros.h"
-#include "ball_chaser/DriveToTarget.h"
-#include <sensor_msgs/Image.h>
 
 // Define a global client that can request services
 ros::ServiceClient client;
@@ -14,58 +13,70 @@ ros::ServiceClient client;
 void drive_robot(float lin_x, float ang_z)
 {
     // TODO: Request a service and pass the velocities to it to drive the robot
+    ROS_INFO_STREAM("Driving the bot in the specified velocity and direction.");
+
+    // Request specified velocity and direction
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
-	
-    // Call the command_robot service and pass the requested joint angles
-    if (!client.call(srv)){
+
+    // Call the command_robot service and pass the specified velocity and direction.
+    if (!client.call(srv))
         ROS_ERROR("Failed to call service command_robot");
-    }	
+
 }
 
+// This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-    int white_pixel = 255;
-    int left_counter = 0;
-    int front_counter = 0;
-    int right_counter = 0;
-	
-    // TODO: 
-    // Loop through each pixel in the image and check if there's a bright white one
-    for (int i = 0; i < img.height * img.step; i += 3) {
-        int position_index = i % (img.width * 3) / 3;
-	
-        if (img.data[i] == white_pixel && img.data[i + 1] == white_pixel && img.data[i + 2] == white_pixel) {
-            if(position_index <= 265) {
-		left_counter += 1;                
-            }
-            if(position_index > 265 && position_index <= 533) {
-		front_counter += 1;               
-            }
-            if(position_index > 533) {
-		right_counter += 1;                
-            }
-	}
-    }
-		
-    // Then, identify if this pixel falls in the left, mid, or right side of the image
-    vector<int> position_counter{left_counter, front_counter, right_counter};
-    int where_to_move = *max_element(position_counter.begin(), position_counter.end());
 
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it.
-    // Request a stop when there's no white ball seen by the camera.
-    if (where_to_move == 0){
-        drive_robot(0.0, 0.5); // When my_robot cannot see a ball, turn left. It is better than completely stop.
+    int white_pixel = 255;
+    
+    int height = img.height; // image height, that is, number of rows
+    int width = img.width; // image width, that is, number of columns
+    int step  = img.step;  // Full row length in bytes
+    // The definition of above parameters could be found at 
+    // http://docs.ros.org/en/melodic/api/sensor_msgs/html/msg/Image.html
+
+
+    vector<int> white_position;  // a vector to store the positions of white pixels
+    int positionId; // The position ID of each white pixel we find. 
+    // This will be stored in  white_position vector.
+
+    // TODO:
+    // (1) Loop through each pixel in the image and check if there's a bright white one
+    for (int i = 0; i < height * step; i += 3) {
+        positionId = i % ( width * 3 ) / 3;
+        if ( img.data[i] == white_pixel && img.data[i + 1] == white_pixel && img.data[i + 2] == white_pixel ) {
+            // Insert the positionId into white_position vector:
+            white_position.push_back(positionId);
+        }         
     }
-    else if (where_to_move == left_counter) {
-	drive_robot(0.0, 0.5);  // This request should drive my_robot left
+    // (2) Identify if the average of these pixels falls in the left, mid, or right side of the image
+    int avg;
+    int sum = 0;
+    int size = white_position.size();
+    for(int k=0; k < size; k++){
+        sum += white_position[k];            
     }
-    else if (where_to_move == front_counter) {
-        drive_robot(0.5, 0.0);  // This request drives my_robot robot forward
+
+    if (size == 0){
+        // Will request a stop when there's no white ball seen by the camera
+        drive_robot(0.0, 0.0);  // This request a stop
     }
-    else if (where_to_move == right_counter) {
-        drive_robot(0.0, -0.5); // This request drives my_robot right
+    else {
+        avg = sum / size;
+
+        // Depending on the white ball position, call the drive_bot function and pass velocities to it
+        if (avg <= width / 3){
+	    drive_robot(0.0, 0.5);  // This request should drive my_robot left
+        }
+        else if (avg >= 2 * width / 3){
+	    drive_robot(0.0, -0.5); // This request drives my_robot right
+        }
+        else{
+	    drive_robot(0.5, 0.0);  // This request drives my_robot robot forward
+        }
     }
 }
 
